1ad603bf414972d28835c3b9d57468fd29c2dbd5
user <bogyung1@naver.com>
2021-08-02 22:28:08 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
         </a>
       </header>
